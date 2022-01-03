a77a6eaaa596e62f75674aa5dd6f01e8895bf70f
Konstantin <iki0@yandex.ru>
2022-01-03 15:20:50 +0400

    Lesson4

---
+++ BuildLib/Building.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BuildLib
+{
+    class Building
+    {
+        private static uint _number = 0;                   //уникальный номер здания
+        private float _height;                             //высота здания
+        private ushort _floorsAmount;                      //количество этажей
+        private ushort _apartmentsAmount;                  //количество квартир в доме
+        private ushort _entrancesAmout;                    //количество подъездов
+
+        public Building() 
+        {
+            _number++;
+        }
+
+        public uint Number
+        {
+            get { return _number; }
+        }
+        
+        public float Height
+        {
+            get { return _height; }
+            set
+            {
+                if (value < 3) value = 3;
+                _height = value;
+            }
+        }
+
+        public ushort FloorsAmount
+        {
+            get { return _floorsAmount; }
+            set
+            {
+                if (value == 0) value = 1;
+                _floorsAmount = value;
+            }
+        }
+
+        public ushort ApartmentsAmount
+        {
+            get { return _apartmentsAmount; }
+            set
+            {
+                if (value < 1) value = 1;
+                _apartmentsAmount = value;
+            }
+        }
+
+        public ushort EntrancesAmout
+        {
+            get { return _entrancesAmout; }
+            set
+            {
+                if (value < 1) value = 1;
+                _entrancesAmout = value;
+            }
+        }
+
+        public float FloorHeight()                         //высота этажа
+        {
+            return _height / _floorsAmount;
+        }
+
+        public ushort ApartmentsInEntrance()               //количество квартир в подъезде
+        {
+            return (ushort)(_apartmentsAmount / _entrancesAmout);
+        }
+
+        public ushort ApartmentsPerFloor()                //количество квартир на этаже
+        {
+            return (ushort)(_apartmentsAmount / _entrancesAmout / _floorsAmount);
+        }
+    }
+}
---
+++ OOP-Lesson4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BuildLib;
+
+namespace OOP_Lesson4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Creator.CreateBuild(5);               //добавить здание с 5 квартирами
+            Creator.CreateBuild(10f, 17);         //добавить здание вывотой 10 и 17 квартирами
+            Creator.CreateBuild(11f, 28, 3);      //добавить здание вывотой 11, 28 квартирами и 3 этажами
+            Creator.CreateBuild(21f, 45, 7, 2);   //добавить здание вывотой 21, 45 квартирами, 7 этажами и 2 подъездами
+
+
+
+            Creator.ShowBuilds();
+
+            Creator.DeleteBuild(3);
+
+            Creator.ShowBuilds();
+
+            Creator.BuildInfo(1);
+            Creator.BuildInfo(2);
+            Creator.BuildInfo(3);
+            Creator.BuildInfo(4);
+            Creator.BuildInfo(5);
+
+            Console.ReadKey();
+        }
+    }
+}
